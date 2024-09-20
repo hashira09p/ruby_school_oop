44590287664431644c18abf1274ea6534cc670d1
Je Rome <jeromeabelida19@gmail.com>
2024-09-20 14:18:37 +0800

    Featured subject added

--- main.rb
+++ main.rb
@@ -1,8 +1,10 @@
 require_relative 'students'
 require_relative 'course'
+require_relative 'subject'
 
 student_id = 1
 course_id = 1
+subject_id = 1
 
 #STUDENT MANAGEMENT
 def add(id_number)
@@ -18,6 +20,7 @@ def add(id_number)
   new_student.phone_number = gets.chomp.to_i
   new_student.save
 
+  puts "Student added successfully!"
   Students.all.each do |element|
     puts element.display
   end
@@ -33,7 +36,7 @@ def delete
 
   if found
     Students.destroy_only_one(delete_student.id)
-    puts "Successfully Destroyed"
+    puts "Student destroyed successfully!"
     puts " "
     puts "These are now the updated list now."
     Students.all.each do |element|
@@ -48,24 +51,78 @@ end
 def add_course(id_number)
   new_course = Course.new
   new_course.id = id_number
-  puts "Please input a name"
+  puts "Please input a course"
   new_course.name = gets.chomp
   new_course.save
 
+  puts "Course added successfully!"
   Course.all.each do |element|
     puts element.display
   end
 end
 
+def delete_course
+  puts "Please input an ID that You want to delete"
+  delete_course = Course.new
+  delete_course.id = gets.chomp.to_i
 
+  found = Course.find_by_id(delete_course.id)
+  failed = "Can't Find the ID" unless found
 
+  if found
+    Course.destroy_only_one(delete_course.id)
+    puts "Course destroyed successfully!"
+    puts " "
+    puts "These are now the updated list now."
+    Course.all.each do |element|
+      puts element.display
+    end
+  else
+    puts failed
+  end
+end
+
+def add_subject(id_number)
+  new_subject = Subject.new
+  new_subject.id = id_number
+  puts "Please input a subject"
+  new_subject.name = gets.chomp
+  new_subject.save
+
+  puts "Subject added successfully!"
+  Subject.all.each do |element|
+    puts element.display
+  end
+end
+
+def delete_subject
+  puts "Please input an ID that You want to delete"
+  delete_subject = Subject.new
+  delete_subject.id = gets.chomp.to_i
+
+  found = Subject.find_by_id(delete_subject.id)
+  failed = "Can't Find the ID" unless found
+
+  if found
+    Subject.destroy_only_one(delete_subject.id)
+    puts "Subject destroyed successfully!"
+    puts " "
+    puts "These are now the updated list now."
+    Subject.all.each do |element|
+      puts element.display
+    end
+  else
+    puts failed
+  end
+end
 
 continue = true
 while continue
   puts "Please input "
   puts "[1] STUDENT MANAGEMENT"
   puts "[2] COURSE MANAGEMENT"
-  puts "[3] EXIT"
+  puts "[3] SUBJECT MANAGEMENT"
+  puts "[4] EXIT"
   action = gets.chomp.to_i
 
   case action
@@ -105,6 +162,8 @@ while continue
 
     end
   when 2
+
+    #If the user choose the COURSE MANAGEMENT
     course_continue = true
     while course_continue
       puts "Please input "
@@ -121,7 +180,7 @@ while continue
         course_id += 1 if user_decision == "Y"
         course_continue = false if user_decision == "N"
       when 2
-        delete
+        delete_course
         puts "Do You want to continue? Y/N"
         user_decision = gets.chomp.upcase
         course_continue = false if user_decision == "N"
@@ -136,8 +195,43 @@ while continue
         puts "INVALID INPUT"
       end
     end
-
   when 3
+
+    #If the user choose the SUBJECT MANAGEMENT
+    subject_continue = true
+    while subject_continue
+      puts "Please input "
+      puts "[1] ADD A NEW SUBJECT"
+      puts "[2] DELETE A SUBJECT"
+      puts "[3] SHOW ALL THE SUBJECT"
+      action = gets.chomp.to_i
+
+      case action
+      when 1
+        add_subject(subject_id)
+        puts "Do You want to continue? Y/N"
+        user_decision = gets.chomp.upcase
+        subject_id += 1 if user_decision == "Y"
+        subject_continue = false if user_decision == "N"
+      when 2
+        delete_subject
+        puts "Do You want to continue? Y/N"
+        user_decision = gets.chomp.upcase
+        subject_continue = false if user_decision == "N"
+      when 3
+        Subject.all.each do |element|
+          puts element.display
+        end
+        puts "Do You want to continue? Y/N"
+        user_decision = gets.chomp.upcase
+        subject_continue = false if user_decision == "N"
+      else
+        puts "INVALID INPUT"
+      end
+    end
+  when 4
+
+  when 5
     #If the user wants to EXIT
     continue = false
   end
--- subject.rb
+++ subject.rb
@@ -1,4 +1,4 @@
-class Course
+class Subject
   attr_accessor :id, :name
 
   @@records = []
@@ -8,21 +8,19 @@ class Course
   end
 
   def self.destroy_only_one(id)
-    found_student = Course.find_by_id(id)
+    found_student = Subject.find_by_id(id)
     @@records.delete(found_student)
-  end
-
-
-  def self.all
-    @@records
   end
 
   def display
     "ID: #{id}, Name: #{name}"
   end
 
+  def self.all
+    @@records
+  end
+
   def self.find_by_id(id_input)
     @@records.find{|element| element.id == id_input}
   end
 end
-
